30145558c93d62e11cb142c1c5b28ed7b73884e1
NicePotato-MS <micahsuessgithub@gmail.com>
2023-12-12 07:17:50 -0600

    perhaps tools pushes on linux?

---
+++ tools/lib/gcc/x86_64-elf/7.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-7.5.0/configure --target=x86_64-elf --prefix=/home/geist/src/toolchains/x86_64-elf-7.5.0-Linux-x86_64 --enable-languages=c,c++ --disable-werror";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
